09ebb58b89f55447352e859f9def0997cae43bc5
Priyanshu Maithani <priyanshumaithani02@gmail.com>
2024-05-17 19:00:57 +0530

    script file moved to html file, and data will store through browser storage

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-let score = {
-  win: 0,
-  lost: 0,
-  tie: 0,
-};
+// let score = {
+//   win: 0,
+//   lost: 0,
+//   tie: 0,
+//   displayScore: function () {
+//     return `No of matches Won: ${score.win},
+//     Lost: ${score.lost},
+//     Tie: ${score.tie}`;
+//   } 
+// };
 
-function playGame(userChoice) {
-  console.log(`You have chosen ${userChoice}`);
+// function playGame(userChoice) {
+//   console.log(`You have chosen ${userChoice}`);
 
-  let randomNumber = Math.random() * 3;
-  let computerChoice;
-  if (randomNumber >= 0 && randomNumber < 1) {
-    computerChoice = 'Bat';
-  } else if (randomNumber >= 1 && randomNumber < 2) {
-    computerChoice = 'Ball';
-  } else {
-    computerChoice = 'Stump';
-  }
+//   let randomNumber = Math.random() * 3;
+//   let computerChoice;
+//   if (randomNumber >= 0 && randomNumber < 1) {
+//     computerChoice = 'Bat';
+//   } else if (randomNumber >= 1 && randomNumber < 2) {
+//     computerChoice = 'Ball';
+//   } else {
+//     computerChoice = 'Stump';
+//   }
 
-  console.log(`Computer choice is ${computerChoice}`);
+//   console.log(`Computer choice is ${computerChoice}`);
 
-  let resultMsg;
-  if (computerChoice === userChoice) {
-    score.tie++;
-    resultMsg = `It's a tie`; 
-  } else if (
-    (computerChoice === 'Ball' && userChoice === 'Bat') ||
-    (computerChoice === 'Bat' && userChoice === 'Stump') ||
-    (computerChoice === 'Stump' && userChoice === 'Ball')
-  ) {
-    score.win++;
-    resultMsg = `User has won`;
-  } else {
-    score.lost++;
-    resultMsg = 'Computer has won';
-  }
+//   let resultMsg;
+//   if (computerChoice === userChoice) {
+//     score.tie++;
+//     resultMsg = `It's a tie`; 
+//   } else if (
+//     (computerChoice === 'Ball' && userChoice === 'Bat') ||
+//     (computerChoice === 'Bat' && userChoice === 'Stump') ||
+//     (computerChoice === 'Stump' && userChoice === 'Ball')
+//   ) {
+//     score.win++;
+//     resultMsg = `User has won`;
+//   } else {
+//     score.lost++;
+//     resultMsg = 'Computer has won';
+//   }
 
-  alert(`You have chosen ${userChoice}. Computer choice is ${computerChoice}.
-  ${resultMsg}
+//   alert(`You have chosen ${userChoice}. Computer choice is ${computerChoice}.
+//   ${resultMsg}
   
-  Won: ${score.win}, Lost: ${score.lost}, Tie: ${score.tie}
-  `);
-}
+//   Won: ${score.win}, Lost: ${score.lost}, Tie: ${score.tie}
+//   `);
+
+//   console.log(score.displayScore);
+// }
